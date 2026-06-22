b7b09d105202ef53a3b7f6e22f142e02512591ee
jitu <jituniloy28@gmail.com>
2026-06-22 20:51:47 +0600

    Add Factorial Recursion Program

---
+++ factorial.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std ; 
+
+int found_factorial(int n ){
+
+    if( n == 0 ) return 1 ;
+
+    return n * found_factorial(n-1) ;
+}
+
+int main (){
+    int n ; 
+    cout << "Enter your number : " ;
+    cin >> n ; 
+    cout << "Factorial of " << n << " " << "is : " << found_factorial(n) << endl ; 
+    return 0 ;
+}
