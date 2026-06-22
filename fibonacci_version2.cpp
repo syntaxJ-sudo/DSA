50569cc2b1273719b573299ebaa605e7706459df
jitu <jituniloy28@gmail.com>
2026-06-22 22:02:59 +0600

    Fibonacci series another version

--- fibonacci_version2.cpp
+++ fibonacci_version2.cpp
@@ -1,22 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void fibonacci( int a , int b , int n ){
+void fibonacci_print( int a , int b , int n ){
 
     if( n == 0 ) return ;
 
     cout << a << " " ;
 
-    fibonacci( b , a + b , n - 1 ) ;
+    fibonacci_print( b , a + b , n - 1 ) ;
 }
 
 int main(){
 
     int n ;
+
     cout << "Print Fibonacci numbers up to: " ; 
+
     cin >> n ;
+
     cout << "Fibonacci Series : " << endl ; 
-    fibonacci( 0 , 1 , n ) ;
+    
+    fibonacci_print( 0 , 1 , n ) ;
 
     return 0 ;
 }
