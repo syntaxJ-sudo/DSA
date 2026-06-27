4f815b74b39b6053ca407ff0bd1f9c557ee791f4
jitu <jituniloy28@gmail.com>
2026-06-27 20:03:43 +0600

    Realloc using C

--- realloc.c
+++ realloc.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-// malloc() allocates memory but does NOT initialize it.
-
+// realloc() is used to increase or decrease the size of previously allocated memory.
 int main(){
 
     int *ptr;
 
-    int n; 
+    int n;
 
     printf("Enter No of gamers:");
     scanf("%d",&n);
 
-    ptr=(int*)malloc(n*sizeof(int));
+    ptr=(int*)calloc(n,sizeof(int));
 
     if(ptr==NULL){
 
         printf("No memory allocation");
         exit(0);
+
 
     }
 
@@ -25,18 +25,34 @@ int main(){
 
     for(int i=0 ; i<n ; i++){
 
-        printf("%d\n",ptr[i]);          // print garbadge values
+        printf("%d\n",ptr[i]);       // print 0, 0 , 0  , ..because------->(calloc()) ; 
 
     }
-
-    printf("Input the serial : ") ;
 
     for(int i=0; i<n ; i++){
 
-        scanf("%d", &ptr[i] ) ;
+        ptr[i]=i+1;             
+        
     }
 
-    printf("After assigining:\n");
+    printf("After assigining position :\n");
+
+    for(int i=0 ; i<n ; i++){
+
+        printf("%d\n",ptr[i]);            // print position no ...1,2,3,4.. ;
+
+    }
+
+    n=n+5;                       // Add 5 more elements to the current size.
+
+    ptr=realloc(ptr,n*sizeof(int)); // Resize the previously allocated memory.
+
+    for(int i=0; i<n ; i++){
+
+        ptr[i]=i+1;
+
+    }
+    printf("After realloc, updated positions :\n");
 
     for(int i=0 ; i<n ; i++){
 
@@ -45,4 +61,4 @@ int main(){
     }
 
     return 0 ;
-}
+}   
